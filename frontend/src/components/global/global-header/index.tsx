273e8355b6c16d469a13c783ec6cb23b619bf568
lazytech614 <derupanjan2021@gmail.com>
2025-06-11 18:15:09 +0530

    completed the header and made it responsive to screen size

--- frontend/src/components/global/global-header/index.tsx
+++ frontend/src/components/global/global-header/index.tsx
@@ -1,10 +1,21 @@
+import { navLinks } from '@/constants/nav-links'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const GlobalHeader = (props: Props) => {
   return (
-    <div className='bg-[var(--accent)] h-[4rem]'>GlobalHeader</div>
+    <div className='bg-[var(--accent)] h-[4rem] px-6 sm:px-20 flex items-center justify-between text-white'>
+      <div>
+        <h1 className='text-2xl font-bold uppercase'>Logo</h1>
+      </div>
+      <div>
+        {navLinks.map((link) => (
+          <Link href={link.href} key={link.name} className='mx-4'>{link.name}</Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/constants/nav-links.ts
@@ -0,0 +1,5 @@
+export const navLinks = [
+    { name: "Home", href: "/" },
+    { name: "Visualize", href: "/visualize" },
+    { name: "About", href: "/about" },
+];
