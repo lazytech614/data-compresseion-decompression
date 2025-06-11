20810a8dccfcec517ed34babb2e36db518694e2c
lazytech614 <derupanjan2021@gmail.com>
2025-06-11 18:26:03 +0530

    updated the selct component eith shadcn UI

--- frontend/src/components/global/algorithm-selector/index.tsx
+++ frontend/src/components/global/algorithm-selector/index.tsx
@@ -1,20 +1,35 @@
-//TODO: Update the type
+import { 
+  Select, 
+  SelectContent, 
+  SelectItem, 
+  SelectTrigger, 
+  SelectValue 
+} from "@/components/ui/select";
 
+
+//TODO: Update the type
 export default function AlgorithmSelector({ algorithms, selected, onChange }: any) {
   return (
     <div className="mb-4">
       <label className="block text-sm font-medium text-[var(--text-light)] mb-1">Choose Algorithm:</label>
-      <select
+      <Select
         value={selected}
-        onChange={(e) => onChange(e.target.value)}
-        className="w-full border border-gray-300 rounded px-2 py-1"
+        onValueChange={(e) => onChange(e)}
       >
-        {algorithms.map((algo: any) => (
-          <option key={algo.id} value={algo.id}>
-            {algo.name}
-          </option>
-        ))}
-      </select>
+        <SelectTrigger className="w-full px-2 py-1">
+          <SelectValue placeholder="Select an algorithm" />
+        </SelectTrigger>
+        <SelectContent className="bg-[var(--card-dark)] text-[var(--text-light)]">
+          {algorithms.map((algo: any) => (
+            <SelectItem 
+              key={algo.id} 
+              value={algo.id}
+            >
+              {algo.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
     </div>
   );
 }
