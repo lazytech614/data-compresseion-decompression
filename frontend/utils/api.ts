9531d7982d63efd6c95095a1bbfae8f528894c5b
lazytech614 <derupanjan2021@gmail.com>
2025-06-18 12:18:01 +0530

    updated the base url

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:5000'; 
+const API_BASE = process.env.NEXT_PUBLIC_API_BASE as string; 
 
 //TODO: update the formdata type
 export async function postCompression(formData: any) {
