9fb70a633373d1678a83bcc911e37dd38458c4ee
lazytech614 <derupanjan2021@gmail.com>
2025-06-19 04:27:12 +0530

    updated the header

--- frontend/src/components/global/global-header/index.tsx
+++ frontend/src/components/global/global-header/index.tsx
@@ -1,20 +1,23 @@
+"use client"
+
 import { NAV_LINKS } from '@/constants/nav-links'
 import Link from 'next/link'
 import React from 'react'
 import {
-  SignInButton,
-  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
 } from '@clerk/nextjs'
 import { Zap } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { usePathname } from 'next/navigation'
 
 const GlobalHeader = () => {
+  const pathname = usePathname();
+
   return (
-      <header className='relative bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900'>
-        <div className="bg-slate-900/50 backdrop-blur-sm border-b border-slate-700">
+    <header className='relative bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900'>
+      <div className="bg-slate-900/50 backdrop-blur-sm border-b border-slate-700">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <Link href="/" className="flex items-center space-x-3">
@@ -23,17 +26,28 @@ const GlobalHeader = () => {
               </div>
               <h1 className="text-xl font-bold text-white">CompressFlow</h1>
             </Link>
+
             <nav className="absolute left-1/2 -translate-x-1/2 hidden md:flex space-x-8">
-              {NAV_LINKS.map((link) => (
-                <Link 
-                  href={link.href} 
-                  key={link.name} 
-                  className="text-slate-300 hover:text-white transition-colors"
-                >
-                  {link.name}
-                </Link>
-              ))}
+              {NAV_LINKS.map((link) => {
+                const isActive = pathname === link.href;
+                return (
+                  <Link
+                    href={link.href}
+                    key={link.name}
+                    className={
+                      `px-4 py-2 font-sm transition-colors rounded-md ${
+                        isActive
+                          ? 'bg-blue-600 text-white shadow-md'
+                          : 'text-slate-300  hover:text-white hover:bg-slate-700'
+                      }`
+                    }
+                  >
+                    {link.name}
+                  </Link>
+                )
+              })}
             </nav>
+
             <div className="flex items-center space-x-4">
               <SignedOut>
                 <Link href={'/auth/sign-in'}>
@@ -49,7 +63,7 @@ const GlobalHeader = () => {
           </div>
         </div>
       </div>
-      </header>
+    </header>
   )
 }
 
